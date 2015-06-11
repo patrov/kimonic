972807ce66e048e23868618eaee66d49cdf1ddeb
Harris <harris.baptiste@gmail.com>
2015-06-11 19:09:10 +0200

    adding callback to template Renderer

--- core/Kimo.TemplateManager.js
+++ core/Kimo.TemplateManager.js
@@ -1,40 +1,45 @@
 define(['vendor.handlebars'], function (HandleBars) {
    var TemplateManager = {
-        error: "",
+        error: "Error while loading the template",
         render: function (path, params) {
-            var deferred = new $.Deferred(),
-                self = this,
-            params = params || {};
-            var placeHolder = params.placeHolder || $("<div>Loading...</div>");
-            
-            require(["text!" + path], function(tpl){ 
-                if(typeof params.dataLoader=="function"){
+            var self = this,
+                params = params || {},
+                placeHolder = params.placeHolder || $("<div>Loading...</div>"),
+                renderCallback = (typeof params.onRender === "function") ? params.onRender : function () {}
+
+
+            require(["text!" + path], function(tpl) {
+                if (typeof params.dataLoader === "function") {
                     params.dataLoader().done(function(data){
                         params.data = data;
                         self.renderer(tpl, placeHolder, params);
+                        renderCallback(placeHolder);
+                    }).fail(function () {
+                        $(placeHolder).html(self.error + " "+path);
                     });
                 }else{
                     self.renderer(tpl, placeHolder, params);
+                    renderCallback(placeHolder);
                 }
             }, function(){
-                self.errorRenderer();
+                self.errorRenderer(placeHolder, path);
             });
             return placeHolder;
         },
-        
+
         renderer: function(tpl, placeHolder, params){
             tpl = HandleBars.compile(tpl);
             var html = tpl(params.data);
             var action = params.renderAction || "html";
             $(placeHolder)[action](html);
         },
-        
-        errorRenderer: function(placeHolder){
-            $(placeHolder).html("<p>Error while loadind the template...</p>");
+
+        errorRenderer: function(placeHolder, path) {
+            $(placeHolder).html(this.error+" "+path);
         }
-    }; 
+    };
     Kimo.TemplateManager = TemplateManager;
     return TemplateManager;
-}); 
+});
 
 
